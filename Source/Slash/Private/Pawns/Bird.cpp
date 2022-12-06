6580d717f2301ffd4f9a3359ca3114bab636f9da
diliupg <unitygamecodersl@gmail.com>
2022-12-06 21:28:31 +0530

    L71 Setting the default pawn

--- Source/Slash/Private/Pawns/Bird.cpp
+++ Source/Slash/Private/Pawns/Bird.cpp
@@ -62,6 +62,17 @@ void ABird::Move( const FInputActionValue& Value )
 	}
 }
 
+void ABird::Look( const FInputActionValue& Value )
+{
+	const FVector2D LookAxisValue = Value.Get<FVector2D>( );
+
+	if ( GetController( ) )
+	{
+		AddControllerYawInput( LookAxisValue.X );
+		AddControllerPitchInput( LookAxisValue.Y );
+	}
+}
+
 // Called every frame
 void ABird::Tick(float DeltaTime)
 { 
@@ -77,6 +88,7 @@ void ABird::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 	if ( UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>( PlayerInputComponent ) )
 	{
 		EnhancedInputComponent->BindAction( MoveAction, ETriggerEvent::Triggered, this, &ABird::Move );
+		EnhancedInputComponent->BindAction( LookAction, ETriggerEvent::Triggered, this, &ABird::Look );
 	}
 
 }
--- Source/Slash/Public/Pawns/Bird.h
+++ Source/Slash/Public/Pawns/Bird.h
@@ -38,7 +38,12 @@ protected:
 	UPROPERTY( EditAnywhere, BlueprintReadOnly, Category = Input )
 	UInputAction* MoveAction;
 
+	UPROPERTY( EditAnywhere, BlueprintReadOnly, Category = Input )
+	UInputAction* LookAction;
+
 	void Move( const FInputActionValue& Value );
+
+	void Look( const FInputActionValue& Value );
 
 private:
 	UPROPERTY(VisibleAnywhere )		
